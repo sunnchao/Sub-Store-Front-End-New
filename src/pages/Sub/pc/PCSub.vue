4a60205ab2cabb3bb32ef4e8da84c0f23f70f152
DesnLee <jiakun.ui@gmail.com>
2023-09-10 01:38:55 +0800

    fix: fix loading bug

--- src/pages/Sub/pc/PCSub.vue
+++ src/pages/Sub/pc/PCSub.vue
@@ -1,35 +1,39 @@
 <template>
   <div>
     <div
-      v-if="!subs.length && !collections.length"
+      v-if="props.isSubLoading || props.isCollectionLoading"
       class="min-h-[320px] flex items-center justify-center"
     >
-      <n-spin
-        v-if="props.isSubLoading || props.isCollectionLoading"
-        size="medium"
-      />
-
-      <n-empty v-else description="暂无订阅" size="huge">
-        <template #icon>
-          <i class="i-solar-confounded-square-bold-duotone block" />
-        </template>
-        <template #extra>
-          <n-button size="small">
-            新增订阅
-          </n-button>
-        </template>
-      </n-empty>
+      <n-spin size="medium" />
     </div>
 
-    <div>
-      <SubPageCardList title="单条订阅" data-type="subs" />
+    <template v-else>
+      <div
+        v-if="!subs.length && !collections.length"
+        class="min-h-[320px] flex items-center justify-center"
+      >
+        <n-empty description="暂无订阅" size="huge">
+          <template #icon>
+            <i class="i-solar-confounded-square-bold-duotone block" />
+          </template>
+          <template #extra>
+            <n-button size="small">
+              新增订阅
+            </n-button>
+          </template>
+        </n-empty>
+      </div>
 
-      <SubPageCardList
-        margin-top="48px"
-        title="组合订阅"
-        data-type="collections"
-      />
-    </div>
+      <div v-else>
+        <SubPageCardList title="单条订阅" data-type="subs" />
+
+        <SubPageCardList
+          margin-top="48px"
+          title="组合订阅"
+          data-type="collections"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
