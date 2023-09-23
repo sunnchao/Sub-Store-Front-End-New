1dd22822224cd6c63e016f8025f04184e440fe02
DesnLee <jiakun.ui@gmail.com>
2023-09-23 21:28:29 +0800

    feat: optimize processors list style

--- src/pages/Editor/pc/components/Processor.vue
+++ src/pages/Editor/pc/components/Processor.vue
@@ -1,26 +1,42 @@
 <template>
-  <n-list>
+  <n-list :style="{ backgroundColor: 'transparent' }">
     <n-list-item
       v-for="processor in filterProcess"
       :key="processor.id"
       :style="{ alignItems: 'flex-start' }"
     >
-      <n-thing
-        v-if="typeof processor.module === 'string'"
-        :title="processor.module"
-      />
+      <n-thing v-if="processor.text" :title="processor.text" />
 
-      <n-thing
-        v-else
-        :title="processor.module.displayName"
-        :description="processor.module.title"
-      >
-        <p>{{ processor.module?.description }}</p>
-        <div class="mt-[16px]">
-          <h2 class="font-medium">
+      <n-thing v-if="processor.module && !processor.text">
+        <div class="mt-[16px] flex items-center justify-between">
+          <div class="flex items-center justify-center">
+            <h1 class="text-[20px] font-medium">
+              {{ processor.module.displayName }}
+            </h1>
+            <n-divider vertical />
+            <h1>{{ processor.module.title }}</h1>
+            <n-tag type="primary" class="ml-[8px]" size="small">
+              {{ processor.module.version }}
+            </n-tag>
+          </div>
+          <div>close</div>
+        </div>
+
+        <p class="my-[16px]">
+          {{ processor.module?.description }}
+        </p>
+
+        <div>
+          <h2 class="text-[16px] font-medium">
             配置参数
           </h2>
-          <n-grid class="mt-[8px]" :cols="3" :x-gap="12" :y-gap="16">
+
+          <n-grid
+            class="mt-[12px]"
+            cols="2 400:3 600:4 900:5 1100:6 1400:7"
+            :x-gap="12"
+            :y-gap="16"
+          >
             <n-grid-item
               v-for="(param, key) in processor.module.params"
               :key="key"
@@ -33,11 +49,9 @@
                       class="i-solar-info-circle-linear ml-[6px] inline-block text-[16px] opacity-48"
                     />
                   </template>
-                  <span>{{
-                    `${param.description} - 数据类型：${
-                      param.dataType
-                    } - 默认值：${param.defaultValue ?? "无"}`
-                  }}</span>
+                  <p>{{ `${param.description}` }}</p>
+                  <p>{{ `数据类型：${param.dataType}` }}</p>
+                  <p>{{ `默认值：${param.defaultValue ?? "无"}` }}</p>
                 </n-popover>
               </p>
 
@@ -51,7 +65,6 @@
                 v-if="param.dataType === 'string'"
                 :value="processor.values[key]"
                 type="textarea"
-                :placeholder="param.description"
                 :autosize="{ minRows: 1, maxRows: 3 }"
                 @update:value="(v) => updateValue(processor.id, key, v)"
               />
@@ -59,7 +72,6 @@
               <n-input-number
                 v-if="param.dataType === 'number'"
                 :value="processor.values[key]"
-                :placeholder="param.description"
                 @update:value="(v) => updateValue(processor.id, key, v)"
               />
 
@@ -67,7 +79,6 @@
                 v-if="param.dataType === 'select'"
                 :value="processor.values[key]"
                 :options="param.options"
-                :placeholder="param.description"
                 @update:value="(v) => updateValue(processor.id, key, v)"
               />
 
@@ -76,7 +87,6 @@
                 multiple
                 :value="processor.values[key]"
                 :options="param.options"
-                :placeholder="param.description"
                 @update:value="(v) => updateValue(processor.id, key, v)"
               />
             </n-grid-item>
@@ -84,9 +94,9 @@
         </div>
       </n-thing>
 
-      <template #suffix>
-        <n-button>Suffix</n-button>
-      </template>
+      <!--      <template #suffix> -->
+      <!--        <n-button>Suffix</n-button> -->
+      <!--      </template> -->
     </n-list-item>
   </n-list>
 </template>
@@ -107,57 +117,56 @@ const emits = defineEmits<{
 const store = useModuleStore();
 const { modules } = storeToRefs(store);
 
-const filterProcess = computed(() =>
-  props.value
-    .filter(p => p.type === 'Script Operator' && p.args.mode === 'link')
-    .map((p) => {
-      const link = p.args.content;
-      const matches = link.match(/^\/api\/module\/([a-zA-Z0-9]+)#?.*$/);
-      if (matches) {
-        const moduleName = matches[1];
-        const module = modules.value.find(m => m.name === moduleName);
-        const valueStr = decodeURI(link).split('#')?.[1];
+type Process = Subscription.Processor & {
+  text: string;
+  module: Modules.BackendModule | null;
+  values: Record<string, any>;
+};
 
-        if (!module) {
-          return {
-            ...p,
-            module: '未知模块',
-            values: {},
-          };
-        }
-        if (!valueStr) {
-          return {
-            ...p,
-            module,
-            values: {},
-          };
-        }
+const filterProcess = computed(() => {
+  const process = props.value.filter(
+    p => p.type === 'Script Operator' && p.args.mode === 'link',
+  );
+  if (!process.length) return [];
 
-        try {
-          const values = JSON.parse(valueStr);
-          console.log(values);
-          return {
-            ...p,
-            module,
-            values,
-          };
-        } catch (e) {
-          console.log(`解析模块【${module.displayName}】参数出错: ${e}`);
-          return {
-            ...p,
-            module,
-            values: {},
-          };
-        }
+  return process.map((p) => {
+    const item: Process = {
+      ...p,
+      text: '',
+      module: null,
+      values: {},
+    };
+
+    const link = p.args.content;
+    const matches = link.match(/^\/api\/module\/([a-zA-Z0-9]+)#?.*$/);
+
+    if (matches) {
+      const moduleName = matches[1];
+      const module = modules.value.find(m => m.name === moduleName);
+      const valueStr = decodeURI(link).split('#')?.[1];
+
+      if (!module) {
+        item.text = '未知模块';
       } else {
-        return {
-          ...p,
-          module: '未知模块',
-          values: {},
-        };
+        item.module = module;
+        if (valueStr) {
+          try {
+            const values = JSON.parse(valueStr);
+            console.log(values);
+            item.values = values;
+          } catch (e) {
+            console.log(`解析模块【${module.displayName}】参数出错: ${e}`);
+            return item;
+          }
+        }
       }
-    }),
-);
+    } else {
+      item.text = '未知模块';
+    }
+
+    return item;
+  });
+});
 
 const updateValue = (id: number, key: string, value: any) => {
   const processCopy = [...props.value];
