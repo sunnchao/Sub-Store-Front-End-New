3862b447a5d38033e1834154534fd95496ad4ca3
sunchaowang <sunchaowang@outlook.com>
2024-05-31 14:17:59 +0800

    feature 增加 functions

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -3,20 +3,20 @@ import axios from 'axios';
 
 import { CONSTANTS } from '../constants';
 
-const getBaseUrl = (): string => {
-  const dataStr = localStorage.getItem(CONSTANTS.BACKEND_API.LS_KEY);
-  const data = dataStr
-    ? (JSON.parse(dataStr) as Settings.BackendApiUrl)
-    : CONSTANTS.BACKEND_API.INITIAL;
-  return (
-    data.apis.find(api => api.name === data.current)?.url ??
-    import.meta.env.VITE_API_URL ??
-    'https://sub.store'
-  );
-};
+// const getBaseUrl = (): string => {
+//   const dataStr = localStorage.getItem(CONSTANTS.BACKEND_API.LS_KEY);
+//   const data = dataStr
+//     ? (JSON.parse(dataStr) as Settings.BackendApiUrl)
+//     : CONSTANTS.BACKEND_API.INITIAL;
+//   return (
+//     data.apis.find(api => api.name === data.current)?.url ??
+//     import.meta.env.VITE_API_URL ??
+//     'https://sub.store'
+//   );
+// };
 
 export const service = axios.create({
-  baseURL: getBaseUrl(),
+  // baseURL: getBaseUrl(),
   timeout: 30000,
 });
 
